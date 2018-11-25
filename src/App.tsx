b1efcbb62341d81a724b0aa658bcd0aa7c2de961
TheD <gianszabernardo@gmail.com>
2018-11-25 17:28:08 -0200

    Change 'App' class name to 'app'

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Welcome from './components/Welcome';
 export default class App extends React.Component {
   public render() {
     return (
-      <div className="App">
+      <div className="app">
         <Welcome />
       </div>
     );
