181ba237e1bcb141c64ab2f49ba9ac41c2b38538
TheD <gianszabernardo@gmail.com>
2018-11-25 17:29:31 -0200

    Init the Welcome component

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,9 +1,34 @@
+// React.
 import * as React from 'react';
 
-const Welcome: React.StatelessComponent<{}> = () => (
-    <button>
-        TESTE
-    </button>
+// Images.
+import jhin from '../img/jhin.jpg';
+import lol_logo from '../img/lol_logo.png';
+
+// Custom Components.
+import Container from './Container';
+import ResponsiveImage from './ResponsiveImage';
+
+// Components.
+const Welcome: React.SFC<{}> = () => (
+    <Container width={100}>
+        <Container width={40} background={`url(${jhin}) center center`} />
+        <Container width={60} background='#18191b'>
+            <h1
+                style={{
+                    color: '#fff',
+                    fontFamily: 'Roboto',
+                    fontSize: '24px',
+                    fontWeight: 600,
+                }}
+            >
+                Welcome to the Jungle
+            </h1>
+            <div>
+                <ResponsiveImage src={lol_logo} alt='League of Legends logo' />
+            </div>
+        </Container>
+    </Container>
 );
 
 export default Welcome;
